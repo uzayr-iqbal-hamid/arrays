03960eb5c47280b7fcd16e032097da2cc7f47829
Uzayr Iqbal Hamid <134723279+uzayr-iqbal-hamid@users.noreply.github.com>
2024-10-09 17:30:21 +0530

    Update Largest Element in Array -Easy.py

--- Largest Element in Array -Easy.py
+++ Largest Element in Array -Easy.py
@@ -1,9 +1,14 @@
 class Solution:
     def largest(self, arr : List[int]) -> int:
-        # code here
         max = 0
         for num in arr:
             if num > max:
                 max = num
         
         return max
+
+# Alternate solution, more efficient
+
+class Solution:
+    def largest(self, arr : List[int]) -> int:
+        return reduce(lambda x, y: x if x > y else y, arr)
